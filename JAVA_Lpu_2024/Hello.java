7735bf4209e988f8e5c379f748d69d6ba8522327
Milan Sihag <milansihag11@gmail.com>
2024-01-18 19:11:22 +0530

    trying

--- JAVA_Lpu_2024/Hello.java
+++ JAVA_Lpu_2024/Hello.java
@@ -1,7 +1,10 @@
 class Hello{
-    public static void main(String args[]){
+    public static void main(String[] argv){
         int num1 = 5;
         int num2 = 10;
         System.out.print(num1 + num2);
+        double num3 = 10d;
+        float num4 = 10.5f;
+
         }
     }
