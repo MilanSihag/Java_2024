f1bba4f09f1965d31d2cb27f3cd8d6b291aeda45
Milan Sihag <milansihag11@gmail.com>
2024-01-18 20:12:52 +0530

    operators

---
+++ JAVA_Lpu_2024/Conditional_statements/01_intro.java
@@ -0,0 +1,18 @@
+package Conditional_statements;
+
+
+class conditional{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        if(a>b){
+            System.out.print("A is grater than B");
+        }
+        else if(a==b){
+            System.out.print("A is equal to B");
+        }
+        else{
+            System.out.print("B is greater than A");
+        }
+    }
+}
--- JAVA_Lpu_2024/Operators/01_intro.java
+++ JAVA_Lpu_2024/Operators/01_intro.java
@@ -2,6 +2,7 @@ package Operators;
 
 class Operators{
     public static void main(String[] args) {
+        //-----Arithmatic operators-----
         int num1 = 12;
         int num2 = 56;
         System.out.println("Add " + (num1+num2));
@@ -9,6 +10,21 @@ class Operators{
         System.out.println("Multiply" + (num1*num2));
         System.out.println("Divide" + (num1/num2));
         System.out.println("Modulus" + (num1%num2));
+        //num1++ is post increment
+        //++num2 is pre increment
+        int output1 = num1++;
+        int output2 = ++num2;
+        System.out.println("output1 ->"+output1+" output2 ->"+output2);
         
+        /*-----Comparison operators-----
+            used to compare two variables/values
+            they will return boolean values("true" or "false")
+            == != > < >= <=
+        */
+
+        /*-----Logical operators---
+            used to compare two conditions
+            && || !
+        */
     }
 }
