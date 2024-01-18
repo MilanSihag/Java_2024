a1a79f3bcdc9b67fa53e8f4856ab15c67ec6714a
Milan Sihag <milansihag11@gmail.com>
2024-01-18 19:37:51 +0530

    operators

---
+++ JAVA_Lpu_2024/Operators/01_intro.java
@@ -0,0 +1,14 @@
+package Operators;
+
+class Operators{
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 56;
+        System.out.println("Add " + (num1+num2));
+        System.out.println("Subtract "+ (num1-num2));
+        System.out.println("Multiply" + (num1*num2));
+        System.out.println("Divide" + (num1/num2));
+        System.out.println("Modulus" + (num1%num2));
+        
+    }
+}
