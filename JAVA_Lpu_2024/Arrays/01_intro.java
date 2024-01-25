a4e0ec2238c013c2e5047737fd99bb47a99037ee
Milan Sihag <milansihag11@gmail.com>
2024-01-25 20:53:54 +0530

    operations on aray

--- JAVA_Lpu_2024/Arrays/01_intro.java
+++ JAVA_Lpu_2024/Arrays/01_intro.java
@@ -3,9 +3,27 @@ package Arrays;
 class intro {
     public static void main(String[] args){
         // int arr[] = {1,2,34,6};
-        int arr[] = new int[4];
-        arr[0] = 12;
-        System.out.println(arr[2]);
-        System.out.println("Array length " + arr.length);
+        // int arr[] = new int[4];
+        // arr[0] = 12;
+        // System.out.println(arr[2]);
+        // System.out.println("Array length " + arr.length);
+
+        // for(int i: arr){
+        //     //for each method for accessing elements of array
+        //     System.out.println(i);
+        // }
+
+        // int roll_no[] = {1,2,3,4,5,6,7,8,9,10};
+        // for(int i: roll_no){
+        //     if(i%2!=0) continue;
+        //     System.out.println(i);
+        // }
+
+        float arr[] = {1.2f,3.4f,23.5f,5.7f,32.8f};
+        float sum = 0f;
+        for(float i : arr){
+            sum+=i;
+        }
+        System.out.println("Sum of array: " + sum);
     }    
 }
---
+++ JAVA_Lpu_2024/Arrays/02_2d_array.java
@@ -0,0 +1,8 @@
+package Arrays;
+
+class array {
+    public static void main(String[] args){
+        int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.print(arr[0][2]);
+    }
+}
