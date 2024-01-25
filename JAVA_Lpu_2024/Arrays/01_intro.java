6dcd30956ada2404a3d98ec8348ef669bb9c9902
Milan Sihag <milansihag11@gmail.com>
2024-01-25 19:47:26 +0530

    array intro

---
+++ JAVA_Lpu_2024/Arrays/01_intro.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+class intro {
+    public static void main(String[] args){
+        // int arr[] = {1,2,34,6};
+        int arr[] = new int[4];
+        arr[0] = 12;
+        System.out.println(arr[2]);
+        System.out.println("Array length " + arr.length);
+    }    
+}
