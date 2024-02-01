0f623d85676e83bdc21f7efb23922118deb75b4d
Milan Sihag <milansihag11@gmail.com>
2024-02-01 20:18:37 +0530

    2d_array Qs

--- JAVA_Lpu_2024/Arrays/02_2d_array.java
+++ JAVA_Lpu_2024/Arrays/02_2d_array.java
@@ -4,19 +4,29 @@ class array {
     public static void main(String[] args){
         int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
         // System.out.print(arr[0][2]);
+        // for(int row = 0; row < arr.length; row++){
+        //     for(int col = 0; col < arr[row].length; col++){
+        //         if(col <= row){
+        //         System.out.print(arr[row][col] + " ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int row = 0; row < arr.length; row++){
+        //     int row_sum = 0;
+        //     for(int col = 0; col < arr[row].length; col++){
+        //         row_sum += arr[row][col];
+        //     }
+        //     System.out.println(row_sum);
+        // }
+
         for(int row = 0; row < arr.length; row++){
+            int row_min = arr[row][0];
             for(int col = 0; col < arr[row].length; col++){
-                if(row == 0 || row == arr.length-1){
-                System.out.print(arr[row][col] + " ");
-                }
-                else if(col == 0 || col == arr[row].length-1){
-                    System.out.print(arr[row][col] + " ");
-                }
-                else{
-                    System.out.print("  ");
-                }
+                if(row_min > arr[row][col]) row_min = arr[row][col];
             }
-            System.out.println();
+            System.out.println(row_min);
         }
     }
 }
