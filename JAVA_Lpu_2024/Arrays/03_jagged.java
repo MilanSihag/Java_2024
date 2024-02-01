827406ce73e1c45e16193b962425741f436ff7a9
Milan Sihag <milansihag11@gmail.com>
2024-02-01 20:57:26 +0530

    jagged array

---
+++ JAVA_Lpu_2024/Arrays/03_jagged.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+class jagged {
+    public static void main(String[] args){
+        // int arr[][] = new int[3][];
+        // arr[0] = new int[3];
+        // arr[1] = new int[4];
+        // arr[2] = new int[2];
+        int arr[][] = {{1,2,3,4},{5,6},{7,8,9}};
+        for(int row = 0; row < arr.length; row++){
+            for(int col = 0; col < arr[row].length; col++){
+                System.out.print(arr[row][col] + " ");
+                }
+            System.out.println();
+        }
+    }
+}
